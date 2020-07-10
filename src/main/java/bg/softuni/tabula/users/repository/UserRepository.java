ec7f843bb62a3b9ac996b392aa99b40853bee061
Lachezar Balev <lachezar.balev@rewe-digital.com>
2020-07-10 21:35:46 +0300

    Initial implementation of the authentication.

---
+++ src/main/java/bg/softuni/tabula/users/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.tabula.users.repository;
+
+import bg.softuni.tabula.users.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
