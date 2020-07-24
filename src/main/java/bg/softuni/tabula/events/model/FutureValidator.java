90cbaf323466b0e73882cd21631e29e6d2d6ec3a
Lachezar Balev <lachezar.balev@rewe-digital.com>
2020-07-24 21:48:01 +0300

    Future validator implemented.

---
+++ src/main/java/bg/softuni/tabula/events/model/FutureValidator.java
@@ -0,0 +1,13 @@
+package bg.softuni.tabula.events.model;
+
+import java.time.LocalDateTime;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class FutureValidator implements ConstraintValidator<Future, LocalDateTime> {
+  @Override
+  public boolean isValid(LocalDateTime value, ConstraintValidatorContext context) {
+    return value != null &&
+        value.isAfter(LocalDateTime.now());
+  }
+}
