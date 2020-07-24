4544b1db34074f9636fd1c573bcfa27b41cc5265
Lachezar Balev <lachezar.balev@rewe-digital.com>
2020-07-24 20:14:46 +0300

    Forbid the stats page for normal user.

--- src/main/java/bg/softuni/tabula/stats/StatsController.java
+++ src/main/java/bg/softuni/tabula/stats/StatsController.java
@@ -1,10 +1,12 @@
 package bg.softuni.tabula.stats;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PreAuthorize("hasRole('ADMIN')")
 public class StatsController {
 
   private StatsService statsService;
