063ff58b53498638b6a009511f7f72d1a4375e74
Lachezar Balev <lachezar.balev@rewe-digital.com>
2020-08-07 18:46:17 +0300

    some post request tests.

--- src/test/java/bg/softuni/tabula/integration/announcement/AnnouncementControllerTest.java
+++ src/test/java/bg/softuni/tabula/integration/announcement/AnnouncementControllerTest.java
@@ -1,22 +1,17 @@
 package bg.softuni.tabula.integration.announcement;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,24 +21,29 @@ public class AnnouncementControllerTest {
   private MockMvc mockMvc;
 
   @Test
-  @WithMockUser(username = "admin", roles={"USER", "ADMIN"})
+  @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
   public void testNewAnnouncement() throws Exception {
-    mockMvc.perform(post("/announcements/save").with(csrf())
-        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-        .param("title", "dasdsadsa fsd fds  fds fds")
-        .param("description", "dasdsad sa dsa dsa d sa fdsfdsfdsfdsfdsfds dsa dsa dsa"))
+    mockMvc.perform(
+        post("/announcements/save").
+        with(csrf()).
+            contentType(MediaType.APPLICATION_FORM_URLENCODED).
+            param("title", "hello, softuni!!!!!!!").
+            param("description", "hello, softuni!!!!!!! hello, softuni!!!!!!! hello, softuni!!!!!!!"))
         .andExpect(status().is3xxRedirection())
         .andExpect(view().name("redirect:/announcements"));
   }
 
-
   @Test
-  @WithMockUser(username = "user", roles={"USER"})
-  public void testGetAnnouncements() throws Exception {
-    mockMvc.perform(get("/announcements"))
-        .andExpect(status().isOk())
-        .andExpect(model().attributeExists("announcements"))
-        .andExpect(view().name("announcement/announcements"));
+  @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+  public void testNewAnnouncementFailed() throws Exception {
+    mockMvc.perform(
+        post("/announcements/save").
+            with(csrf()).
+            contentType(MediaType.APPLICATION_FORM_URLENCODED).
+            param("title", "hello, softuni!!!!!!!").
+            param("description", "short"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(view().name("redirect:/announcements/new"));
   }
 
 }
